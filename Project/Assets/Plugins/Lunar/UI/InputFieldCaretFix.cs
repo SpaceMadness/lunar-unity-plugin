d0ae527f5d4cd8446be8b4be5436293da6ce1ec2
Alex Lementuev <a.lementuev@gmail.com>
2015-08-01 13:30:12 -0700

    Progress

---
+++ Project/Assets/Plugins/Lunar/UI/InputFieldCaretFix.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using System.Collections;
+
+namespace LunarPluginInternal
+{
+    /*
+     * This is a dirty hack to fix input field caret alignment issue when using custom fonts.
+     * Last checked with Unity 5.1.2f1
+     */
+
+    [RequireComponent(typeof(InputField))]
+    class InputFieldCaretFix : MonoBehaviour, ISelectHandler
+    {
+        public float offset = 0;
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            if (offset != 0)
+            {
+                InputField inputField = gameObject.GetComponent<InputField>();
+                RectTransform caretTransform = (RectTransform) inputField.transform.Find(gameObject.name + " Input Caret");
+                if (caretTransform != null)
+                {
+                    MoveCaret(caretTransform);
+                }
+                else
+                {
+                    // need to skip 1 frame
+                    StartCoroutine(MoveCaretRoutine(inputField));
+                }
+            }
+        }
+
+        IEnumerator MoveCaretRoutine(InputField inputField)
+        {
+            yield return null; // skip a frame
+
+            string caretName = gameObject.name + " Input Caret";
+
+            RectTransform caretTransform = (RectTransform) inputField.transform.Find(caretName);
+            if (caretTransform != null)
+            {
+                MoveCaret(caretTransform);
+            }
+            else
+            {
+                Debug.LogWarning("Can't find RectTransform of '" + caretName + "': caret might be misaligned!");
+            }
+        }
+
+        void MoveCaret(RectTransform caretTransform)
+        {
+            Vector3 localPosition = caretTransform.localPosition;
+            localPosition.y += offset;
+            caretTransform.localPosition = localPosition;
+        }
+    }
+}
---
+++ Project/Assets/Plugins/Lunar/UI/SelectOnAwake.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using System.Collections;
+
+namespace LunarPluginInternal
+{
+    class SelectOnAwake : MonoBehaviour
+    {
+        #pragma warning disable 0649
+        public InputField selectable;
+        #pragma warning restore 0649
+
+        void OnEnable()
+        {
+            StartCoroutine(Select());
+        }
+
+        IEnumerator Select()
+        {
+            yield return null;
+            EventSystem.current.SetSelectedGameObject(selectable.gameObject);
+        }
+    }
+}
