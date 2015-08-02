ee08e21d8eb7dc0293fb0018ed73f4a99ef9ddf9
Alex Lementuev <a.lementuev@gmail.com>
2015-08-01 17:50:02 -0700

    Progress

---
+++ Project/Assets/Plugins/Lunar/UI/CommandInputField.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using System.Collections;
+
+[RequireComponent(typeof(InputField))]
+class CommandInputField : MonoBehaviour
+{
+    private InputField m_inputField;
+
+    #region MonoBehaviour callbacks
+
+    void OnEnable()
+    {
+        m_inputField = GetComponent<InputField>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            print(m_inputField.text);
+            m_inputField.text = "";
+        }
+    }
+
+    #endregion
+}
