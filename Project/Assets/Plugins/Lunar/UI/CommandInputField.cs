6c51eef0e3ee7994d8ce0ca9847bb6a0a7a1dff0
Alex Lementuev <a.lementuev@gmail.com>
2015-08-01 19:37:30 -0700

    Progress

--- Project/Assets/Plugins/Lunar/UI/CommandInputField.cs
+++ Project/Assets/Plugins/Lunar/UI/CommandInputField.cs
@@ -4,67 +4,101 @@ using UnityEngine.EventSystems;
 
 using System.Collections;
 
-[RequireComponent(typeof(InputField))]
-class CommandInputField : MonoBehaviour
+namespace LunarPluginInternal
 {
-    private InputField m_inputField;
-
-    #region MonoBehaviour callbacks
-
-    void OnEnable()
+    public interface ICommandInputFieldDelegate
     {
-        // disable tab characters
-        this.InputField.onValidateInput = delegate(string text, int charIndex, char addedChar)
+        void ExecuteCommand(CommandInputField input, string commandLine);
+        void AutoComplete(CommandInputField input, string commandLine, int index, bool doubleTab);
+    }
+
+    [RequireComponent(typeof(InputField))]
+    public class CommandInputField : MonoBehaviour
+    {
+        private InputField m_inputField;
+
+        #region MonoBehaviour callbacks
+
+        void OnEnable()
         {
-            if (addedChar == '\t')
+            // disable tab characters
+            this.InputField.onValidateInput = delegate(string text, int charIndex, char addedChar)
             {
-                return '\0';
+                if (addedChar == '\t')
+                {
+                    return '\0';
+                }
+
+                return addedChar;
+            };
+        }
+
+        void Update ()
+        {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                Submit();
             }
-
-            return addedChar;
-        };
-    }
-
-    void Update ()
-    {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            Submit();
+            else if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                AutoComplete();
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Tab))
+
+        #endregion
+
+        #region Input
+
+        void Submit()
         {
-            AutoComplete();
+            print(this.InputField.text);
+            this.InputField.text = "";
         }
-    }
 
-    #endregion
-
-    #region Input
-
-    void Submit()
-    {
-        print(this.InputField.text);
-        this.InputField.text = "";
-    }
-
-    void AutoComplete()
-    {
-    }
-
-    #endregion
-
-    #region Properties
-
-    private InputField InputField
-    {
-        get
+        void AutoComplete()
         {
-            if (m_inputField == null)
-                m_inputField = GetComponent<InputField>();
-
-            return m_inputField;
+            if (this.Delegate != null)
+            {
+                this.Delegate.AutoComplete(this, this.Text, this.Text.Length, false);
+            }
         }
-    }
 
-    #endregion
+        #endregion
+
+        #region Properties
+
+        public ICommandInputFieldDelegate Delegate
+        {
+            get; set;
+        }
+
+        public string Text
+        {
+            get { return this.InputField.text; }
+            set
+            {
+                this.InputField.text = StringUtils.NonNullOrEmpty(value);
+                this.InputField.MoveTextEnd(false);
+            }
+        }
+
+        private int CaretPos
+        {
+            get { return this.InputField.caretPosition; }
+            set { this.InputField.caretPosition = Mathf.Clamp(value, 0, this.Text.Length); }
+        }
+
+        private InputField InputField
+        {
+            get
+            {
+                if (m_inputField == null)
+                    m_inputField = GetComponent<InputField>();
+
+                return m_inputField;
+            }
+        }
+
+        #endregion
+    }
 }
--- Project/Assets/Plugins/Lunar/UI/TerminalCanvas.cs
+++ Project/Assets/Plugins/Lunar/UI/TerminalCanvas.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TerminalCanvas : MonoBehaviour {
+namespace LunarPluginInternal
+{
+    public class TerminalCanvas : MonoBehaviour, ITerminalView, ICommandInputFieldDelegate
+    {
+        [SerializeField]
+        CommandInputField input; // FIXME
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private Terminal m_terminal;
+
+        void OnEnable()
+        {
+            if (m_terminal == null)
+                m_terminal = new FormattedTerminal(1024); // FIXME
+
+            if (input == null)
+            {
+                Debug.LogError("Missing command input");
+                return;
+            }
+
+            input.Delegate = this;
+        }
+
+        #region ICommandInputFieldDelegate
+
+        public void ExecuteCommand(CommandInputField input, string commandLine)
+        {
+            App.ExecCommand(commandLine, true);
+        }
+
+        public void AutoComplete(CommandInputField input, string commandLine, int index, bool doubleTab)
+        {
+            string line = m_terminal.DoAutoComplete(commandLine, index, doubleTab);
+            if (line != null)
+            {
+                input.Text = line;
+            }
+        }
+
+        #endregion
+    }
 }
