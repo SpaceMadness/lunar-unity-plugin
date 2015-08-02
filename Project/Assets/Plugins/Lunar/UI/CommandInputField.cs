a1cf155a3a2ad47439348bf1847e74c7a11678e5
Alex Lementuev <a.lementuev@gmail.com>
2015-08-01 18:02:07 -0700

    Progress

--- Project/Assets/Plugins/Lunar/UI/CommandInputField.cs
+++ Project/Assets/Plugins/Lunar/UI/CommandInputField.cs
@@ -13,16 +13,47 @@ class CommandInputField : MonoBehaviour
 
     void OnEnable()
     {
-        m_inputField = GetComponent<InputField>();
+        if (m_inputField == null)
+        {
+            m_inputField = GetComponent<InputField>();
+
+            // disable tab characters
+            m_inputField.onValidateInput += delegate(string text, int charIndex, char addedChar)
+            {
+                if (addedChar == '\t')
+                {
+                    return '\0';
+                }
+
+                return addedChar;
+            };
+        }
     }
 
     void Update ()
     {
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            print(m_inputField.text);
-            m_inputField.text = "";
+            Submit();
         }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            AutoComplete();
+        }
+    }
+
+    #endregion
+
+    #region Input
+
+    void Submit()
+    {
+        print(m_inputField.text);
+        m_inputField.text = "";
+    }
+
+    void AutoComplete()
+    {
     }
 
     #endregion
