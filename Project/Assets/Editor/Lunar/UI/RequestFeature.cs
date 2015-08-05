a8e17323464c3b022221b4e15a13660ccb8ebffc
Alexandr Korsak <alex.korsak@gmail.com>
2015-08-05 22:46:12 +0300

    Add RequestFeature button

---
+++ Project/Assets/Editor/Lunar/UI/RequestFeature.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+namespace LunarEditor {
+	class RequestFeature {
+		internal static void Open() {
+			Application.OpenURL("https://github.com/SpaceMadness/lunar-unity-plugin/issues/new");
+		}
+	}
+}
